6e934ac6289b5bff0e98949ef97b2a5710669f8d
Diego Medina <diego.medina@globallogic.com>
2022-04-25 19:38:28 -0300

    Desafio entregable 4

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 import './ItemListContainer.css'
 
 function ItemListContainer({ greeting }) {
+  function dummy() {
+    console.log('dummy prop function');
+  }
+
   return (
     <div className='list-item-container'>
-        <p>{ greeting }</p>
+        <ItemCount initial={0} stock={5} onAdd={dummy} />
     </div>
   )
 }
